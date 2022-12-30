8af65e8cf848296a79f76f7294ac540682731e52
Devan74 <raghavadeva1903@gmail.com>
2022-12-30 22:00:07 +0530

    last

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const connectDB = require("./db/db_connection");
 const auth_route = require("./routes/auth_route");
 const urlPrivate_route = require("./routes/urlPrivate_route");
 const redirectUrl = require("./routes/urlRedirect_route");
-
+require ('dotenv').config();
 app.use("/", redirectUrl);
 app.use("/api/auth", auth_route);
 app.use("/api/urlPrivate", urlPrivate_route);
@@ -19,6 +19,7 @@ connectDB()
   .then(() =>
     app.listen(process.env.PORT, () => {
       console.log("Server-ON");
+      console.log(process.env.PORT);
     })
   )
   .catch((e) => console.log(e, " err-index.js"));
